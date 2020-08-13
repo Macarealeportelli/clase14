c8d2d349642adb962db9eadf953c035f7b80dea9
Macarealeportelli <macarenarealeportelli@gmail.com>
2020-08-12 22:00:40 -0300

    practica de funciones JS

---
+++ index.js
@@ -0,0 +1,47 @@
+//ejercicio 1
+const sumar = (nro1, nro2) => {
+    return nro1 + nro2
+}
+
+console.log (sumar (2,3))
+console.log (sumar (1.2, 3.4))
+console.log (sumar (3, -5))
+
+//ejercicio 2
+
+const restar = (nro1, nro2) => {
+    return nro1 - nro2
+}
+
+console.log (restar (3,2))
+console.log (restar (10, 5.5))
+console.log (restar (3, 5))
+
+//ejercicio 3
+
+const multiplicar = (nro1, nro2) => {
+    return nro1 * nro2
+}
+
+console.log (multiplicar (2,3))
+console.log (multiplicar (4, 0.5))
+
+//ejercicio 4
+const dividir = (nro1, nro2) => {
+    return nro1 / nro2
+}
+
+console.log (dividir (2,3))
+console.log (dividir (1.2, 3.4))
+console.log (dividir (3, -5))
+
+//ejercicio 5
+
+const calcularAreaTriangulo = (base , altura) => {
+    let multiplica = base*altura
+    return multiplica/2
+}
+
+console.log (calcularAreaTriangulo(3, 4))
+console.log (calcularAreaTriangulo(5, 6))
+
